7dca9f2c883de8d1dfc415c8c7344a990e37fc13
RexVamanhole622 <85119072+RexVamanhole622@users.noreply.github.com>
2022-02-20 19:00:29 +0800

    assignment 4

--- assignment_4/src/menuapp/items.controller.js
+++ assignment_4/src/menuapp/items.controller.js
@@ -2,7 +2,7 @@
 'use strict';
 
 angular.module('MenuApp')
-.controller('itemsController', ItemsController);
+.controller('ItemsController', ItemsController);
 
 ItemsController.$inject = ['items'];
 function ItemsController(items) {
--- assignment_4/src/routes.js
+++ assignment_4/src/routes.js
@@ -1,46 +1,42 @@
 (function () {
-'use strict';
-
-angular.module('MenuApp')
-.config(RoutesConfig);
-
-RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
-function RoutesConfig($stateProvider, $urlRouterProvider) {
-
-$urlRouterProvider.otherwise('/');
+    'use strict';
     
-$stateProvider
-
-//home page
-.state('home', {
-    url: '/',
-    templateUrl: 'src/menuapp/templates/home.template.html'
-})
-
-//categories page
-.state('categories', {
-    url: '/categories',
-    templateUrl: 'src/menuapp/templates/categories.template.html',
-    controller:  'CategoriesController as catCrtl',
-    resolve: {
-        items: ['MenuDataService', function (MenuDataService) {
+    angular.module('MenuApp')
+    .config(RoutesConfig);
+    
+    RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+    function RoutesConfig($stateProvider, $urlRouterProvider) {
+    
+      $urlRouterProvider.otherwise('/');
+    
+      $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'src/menuapp/templates/home.template.html'
+      })
+      .state('categoryList', {
+        url: '/category-list',
+        templateUrl: 'src/menuapp/templates/category-list.template.html',
+        controller: 'CategoryListController as cateLstCtrl',
+        resolve: {
+          items: ['MenuDataService', function (MenuDataService) {
             return MenuDataService.getAllCategories();
-        }]
+          }]
+        }
+      })
+      .state('items', {
+        url: '/items/{category}',
+        templateUrl: 'src/menuapp/templates/items.template.html',
+        controller: 'ItemsController as itemsCtrl',
+        resolve: {
+          items: ['MenuDataService', '$stateParams', function (MenuDataService, $stateParams) {
+            return MenuDataService.getItemsForCategory($stateParams.category);
+          }]
+        }
+      });
+    
+    
     }
-})
-
-//items-list
-.state('items', {
-    url:'/items/{category}',
-    templateUrl: 'src/menuapp/templates/items.template.html',
-    controller: 'ItemsController as itemsCtrl',
-    resolve: {
-        items: ['MenuDataService', function (MenuDataService, $stateParams) {
-        return MenuDataService.getItemsForCategory($stateParams.category);
-        }]
-    }
-});
-
-}
-
-})();
+    
+    })();
+    
